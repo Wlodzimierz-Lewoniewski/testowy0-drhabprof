82226fc8482f66802a6b0b971e215c6d9b8e53e7
drhabprof <117540964+drhabprof@users.noreply.github.com>
2022-11-05 21:43:05 +0100

    Update main.py

--- main.py
+++ main.py
@@ -6,8 +6,9 @@ for x in range(sentences_count):
     sentences.append(a)
 
 for word in words:
-    result=[]
+    result={}
     for num,sentence in enumerate(sentences):
         tokens=sentence.split()
-        result.append(tokens.count(word))
+        if word in tokens:
+            result[].append(tokens.count(word))
     print (result)
