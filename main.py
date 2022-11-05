780fa9e45dc8325ccf48cf1815e9831cbc5043cf
drhabprof <117540964+drhabprof@users.noreply.github.com>
2022-11-05 21:09:06 +0100

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ words_count = int(input())
 words=[]
 for x in range(words_count):
   a=input()
-  words.append(a)
+  words.append(a.strip())
 
 for word in words:
   if len(word)>2: print (word,len(word))
